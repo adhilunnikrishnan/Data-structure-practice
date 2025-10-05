a5c38386b532cc61a534d538c6920ea3f0bc868e
adhilunnikrishnan <adhilunnikrishnanofficial@gmail.com>
2025-10-05 20:34:46 +0530

    practice:remove duplicate in JS

---
+++ Array/removeduplicate.js
@@ -0,0 +1,7 @@
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+let nums = [1, 2, 3, 2 , 4, 1, 5, 6 , 3];
+console.log("Original:", nums);
+console.log("Without Duplicates:", removeDuplicates(nums));
